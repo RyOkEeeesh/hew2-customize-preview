454d9acafd0d20b85603933c5977b4cd0c223eab
kaji <dafe00842202@outlook.jp>
2026-05-13 02:26:06 +0900

    update

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/hew2-customize-preview/",
+  base: './',
+  build: {
+    outDir: 'hew2-customize-preview',
+  },
   plugins: [react()],
 });
